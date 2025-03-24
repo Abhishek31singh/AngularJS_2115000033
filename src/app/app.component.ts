eb28ab0642884811c4757d53c24ff47ffea3d85f
Abhishek31singh <arya31thakur@gmail.com>
2025-03-24 11:20:10 +0530

    Displaying of logo using property binding technique

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  //========UC1=============//
   title = 'HelloWorld-App';
+  // //============UC2===============//
   message:string='Hello From BridgeLabz';
+  //============UC3================//
+  bridgeLabzLogo: string="https://media.licdn.com/dms/image/v2/C510BAQEYyeT3P0H_mw/company-logo_200_200/company-logo_200_200/0/1630611149363/bridgelabz_com_logo?e=2147483647&v=beta&t=TFgRiaA55f57NqQFnUbnsKu3mQo7c-LOfD8_iNSgtNM"
 }
